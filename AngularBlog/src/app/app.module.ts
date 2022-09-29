fd97faf98b607d0fd9e4e8fa5b1654b91d4df474
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-09-29 09:31:02 -0300

    menu title

---
+++ AngularBlog/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
