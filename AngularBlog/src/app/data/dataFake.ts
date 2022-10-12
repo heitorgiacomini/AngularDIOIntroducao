a9cae949d822e6f1ac5a47c39c9f836bd2530f85
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-11 22:04:57 -0300

    Deixando a content Page Dinâmica

--- AngularBlog/src/app/data/dataFake.ts
+++ AngularBlog/src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     id: 1,
-    photo: "",
+    photo: "https://image.shutterstock.com/image-vector/img-file-document-icon-260nw-1363855613.jpg",
     title: "saiu novo filme",
     description : "ola"
   },
   {
-    id: 1,
-    photo: "",
-    title: "saiu novo filme",
+    id: 2,
+    photo: "https://image.shutterstock.com/image-vector/img-file-document-icon-260nw-1363855613.jpg",
+    title: "saiu velho filme",
     description : "ola"
   }
 ];
--- AngularBlog/src/app/pages/content/content.component.ts
+++ AngularBlog/src/app/pages/content/content.component.ts
@@ -1,3 +1,4 @@
+import { dataFake } from './../../data/dataFake';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -14,15 +15,27 @@ export class ContentComponent implements OnInit {
   Title!: string;
   @Input()
   Description!: string;
+  private id!:string | null;
 
   constructor(
     private _route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this._route.paramMap.subscribe(value =>
-      console.log(value.get("id"))
+    this._route.paramMap.subscribe(value =>{
+      this.id =  value.get("id");
+      this.setValuesToComponent(this.id!)
+    }
     );
   }
 
+  setValuesToComponent(id:string){
+    const result = dataFake.filter(article => article.id.toString() == id)[0]
+
+    if(result){
+      this.photoCover = result.photo;
+      this.Title = result.title;
+      this.Description = result.description;
+    }
+  }
 }
