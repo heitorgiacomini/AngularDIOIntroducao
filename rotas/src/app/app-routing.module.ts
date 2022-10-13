054b8493e37c702cea85d1c30b2a04c6d3d23ed7
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-13 16:29:54 -0300

    Recuperando Parametros das Rotas

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: TitleComponent, pathMatch: 'full' },
-  {path: 'portfolio', component: CardComponent, pathMatch: 'prefix' },
+  {path: 'portfolio/:id', component: CardComponent, pathMatch: 'prefix' },
   {path: '**', redirectTo: ''},
 ];
 
