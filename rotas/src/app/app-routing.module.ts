97af34f57a5922f373adbd6852bc77f49d8189ab
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-13 16:17:58 -0300

    Navegando entre páginas com RouterLink

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CardComponent } from './pages/portifolio/card/card.component';
+import { TitleComponent } from './pages/index/title/title.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TitleComponent, pathMatch: 'full' },
+  {path: 'portfolio', component: CardComponent, pathMatch: 'prefix' },
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
