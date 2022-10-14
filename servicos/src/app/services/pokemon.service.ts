f4d3636233b7e568a4fd95216bc043a3ba4b802b
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-14 13:59:36 -0300

    Trabalhando com Módulo HTTP

--- servicos/src/app/services/pokemon.service.ts
+++ servicos/src/app/services/pokemon.service.ts
@@ -1,16 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
   private baseUrl!:string;
-  constructor() {
+  private pokeData!:any;
+
+  constructor(
+    private _http: HttpClient
+  ) {
     this.baseUrl = environment.pokeAPi;
   }
 
   getPokemon(pokemonName: string){
-
+   this.pokeData = this._http.get(`${this.baseUrl}${pokemonName}`);
   }
 }
