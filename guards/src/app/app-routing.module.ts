f21fcfbfc4cae71fa58b1a88aacae84e6b7a9597
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-18 14:21:24 -0300

    Entendendo a anatomia de um guard

--- guards/src/app/app-routing.module.ts
+++ guards/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { AuthGuard } from './guards/auth.guard';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', redirectTo: '' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'produtos', component: ProdutosComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
