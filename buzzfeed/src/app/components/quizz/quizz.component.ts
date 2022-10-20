8b5f5fa649bad0bd04a03fb9cb5cb5610229e0d7
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-20 12:00:15 -0300

    Criando as propriedades logicas

--- buzzfeed/src/app/components/quizz/quizz.component.ts
+++ buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  title: string = '';
+
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
